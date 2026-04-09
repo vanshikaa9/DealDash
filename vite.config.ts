fbe8dc54781babb80b182db25f9002daa6ebc0c0
Vanshika Mishra <csds23135@glbitm.ac.in>
2026-04-09 17:31:41 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import sourceMapperPlugin from "./source-mapper/src/index";
-import { devToolsPlugin } from "./dev-tools/src/vite-plugin";
-import { fullStoryPlugin } from "./fullstory-plugin";
+
+export default defineConfig({
+  plugins: [react()],
+})
